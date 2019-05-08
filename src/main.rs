e86c6949b043775a2d6091d51c8cc79cd02a8424
wangluning <wangluning@jianbing.com>
2019-05-08 22:05:09 +0800

    add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate actix;
+use actix::{msgs, Actor, Addr, Arbiter, Context, System};
+
 fn main() {
+    let system = System::new("test");
+    system.run();
     println!("Hello, world!");
 }
