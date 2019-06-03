c5a1a01385d30e55f45941641e1bdb72520b9dad
deadwind4 <rsl4@foxmail.com>
2019-06-03 19:51:39 +0800

    add update text shell

---
+++ src/sbin/update-text.sh
@@ -0,0 +1,11 @@
+pk_column=1
+TAB=$'\t'
+sed -i 's/\t/\x1/g' origin
+update=`sed 's/\t/\x1/g' update`
+new_lines=($update)
+for new_line in ${new_lines[@]}
+do
+    #用``就不行用$()可以
+    pk=$(echo $new_line | awk -F '\x1' "{print \$${pk_column}}")
+    sed -i "s/^${pk}\x1.*$/${new_line}/" origin
+done
