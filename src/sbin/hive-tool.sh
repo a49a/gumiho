fd337acbf167745b61b1179d8eeb1ed7fa57b2c7
wang <rsl4@foxmail.com>
2019-06-08 20:52:35 +0800

    fixed pom

--- src/sbin/hive-tool.sh
+++ src/sbin/hive-tool.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-beeline -u jdbc:hive2://localhost:10000 -n foo -p foo << EOF
+beeline -u jdbc:hive2://localhost:10000 -n foo -p foo -e << EOF
     LOAD DATA LOCAL INPATH '' OVERWRITE INTO TABLE foo
 EOF
