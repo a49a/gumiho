de5414525dab57876d6b217173934d314e12f1e0
wang <rsl4@foxmail.com>
2019-06-08 19:08:10 +0800

    remove conflict

--- src/main/scala/org/gumiho/Gumiho.scala
+++ src/main/scala/org/gumiho/Gumiho.scala
@@ -3,11 +3,7 @@ import org.slf4j.LoggerFactory
 
 object Gumiho {
     def main(args: Array[String]) = {
-<<<<<<< HEAD:src/main/scala/org/gumiho/Main.scala
-        val logger: Logger = LoggerFactory.getLogger(Main.getClass)
-=======
         val logger: Logger = LoggerFactory.getLogger(Gumiho.getClass)
->>>>>>> dev:src/main/scala/org/gumiho/Gumiho.scala
         logger.info("test")
     }
 }
