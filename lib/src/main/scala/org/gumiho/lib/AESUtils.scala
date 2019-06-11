d40f4ac54967fb35b4f3ea464ed9e5d2416111a4
deadwind4 <rsl4@foxmail.com>
2019-06-11 21:01:00 +0800

    add AES and HASH Utils

---
+++ lib/src/main/scala/org/gumiho/lib/AESUtils.scala
@@ -0,0 +1,51 @@
+package org.gumiho.lib
+
+import java.util.{Base64, UUID}
+
+import javax.crypto.Cipher
+import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
+
+object AESUtils {
+    val KEY_ALGORITHM = "AES"
+    val CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding"
+    val IV = "AAAAAAAAAAAAAAAA"
+
+    def encrypt(content: String, key: String, iv: String) = {
+        val cipher = Cipher.getInstance(CIPHER_ALGORITHM)
+        cipher.init(Cipher.ENCRYPT_MODE, genSecretKey(key), genIv(iv))
+        val result = cipher.doFinal(content.getBytes())
+        new String(Base64.getEncoder.encode(result), "UTF-8")
+    }
+
+    def decrypt(content: String, key: String, iv: String) = {
+        val cipher = Cipher.getInstance(CIPHER_ALGORITHM)
+        //genIv(iv)
+        cipher.init(Cipher.DECRYPT_MODE, genSecretKey(key), genIv(iv))
+        val result = cipher.doFinal(Base64.getDecoder.decode(content))
+        new String(result, "UTF-8")
+    }
+
+    private def genSecretKey(key: String) = {
+        new SecretKeySpec(key.getBytes(), KEY_ALGORITHM)
+    }
+
+    private def genIv(iv: String) = {
+        //val iv = key.substring(0, 16).getBytes()
+        new IvParameterSpec(iv.getBytes())
+    }
+
+    def genKey(): String = {
+        val uuid = UUID
+            .randomUUID()
+            .toString()
+            .replaceAll("-", "")
+            .toLowerCase()
+        uuid
+    }
+
+    private def oidGenIv(key: String) = {
+        val iv = key.substring(0, 16).getBytes()
+        new IvParameterSpec(iv)
+    }
+
+}
---
+++ lib/src/main/scala/org/gumiho/lib/HashUtils.scala
@@ -0,0 +1,19 @@
+package org.gumiho.lib
+
+import java.security.MessageDigest
+
+object HashUtils {
+    def getMD5Hex32(content: String) = {
+        val digest = MessageDigest.getInstance("MD5")
+        val hashBytes = digest.digest(content.getBytes())
+        hashBytes.map((x) => {
+            val hex = Integer.toHexString(x & 0xFF)
+            hex match {
+                case x if x.length == 1 => s"0$x"
+                case _ => hex
+            }
+        }).reduce((x, y) => {
+            x + y
+        })
+    }
+}
