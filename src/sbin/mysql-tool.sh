cc8b4dc6331ad20d40fba504cfc0bdd61ac40aac
deadwind4 <rsl4@foxmail.com>
2019-06-11 11:15:25 +0800

    add get update field func

--- src/sbin/mysql-tool.sh
+++ src/sbin/mysql-tool.sh
@@ -7,13 +7,20 @@ DB="foo"
 TABLE="foo"
 updated_at="update_time"
 
-#table_name,update_field
+#$1表明,$2增量更新字段
 incremental_sync() {
     time=`date +%F -d -1day`
     SQL="SELECT * FROM $1 WHERE $2 > ${time}"
     mysql -h${HOST}-u${USER} -p{PASSWORD} ${DB} -e ${SQL} >> "$1_${time}.csv"
 }
-
+#$1库名
+get_update_field() {
+    SQL="SELECT table_name, column_name FROM information_schema.columns \
+    WHERE table_schema=$1 AND \
+    column_default='CURRENT_TIMESTAMP' AND \
+    extra='on update CURRENT_TIMESTAMP'"
+    mysql -h${HOST}-u${USER} -p{PASSWORD} ${DB} -e ${SQL}
+}
 
 
 
