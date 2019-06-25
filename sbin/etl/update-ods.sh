a998e3f6ac903b3b400d576f8f1ac4f6f37f5b76
deadwind4 <rsl4@foxmail.com>
2019-06-25 14:29:05 +0800

    add clean and download func in ods etl sh

--- sbin/etl/update-ods.sh
+++ sbin/etl/update-ods.sh
@@ -7,22 +7,58 @@ now_day=$(date +"%Y%m%d")
 conf="${base_dir}/conf/$1.conf"
 update_conf=${base_dir}/conf/update.conf
 update_src_dir=${hdfs_base}/ods/${database}/update/${now_day}
-old_data_dir=${hdfs_base}/dwd/${database}
-data_download_dir=/tmp/etl/${database}
-update_download_dir="${data_download_dir}/update"
+full_dir=${hdfs_base}/dwd/${database}
+full_tmp_dir=/tmp/etl/${database}
+update_tmp_dir="${data_download_dir}/update"
 new_data_upload_dir=${hdfs_base}/tao/dwd/${database}
-delimit="\001"
-update_file_delimit="\t"
+delimiter="\001"
+old_delimiter="\t"
 meta_dir="${base_dir}/meta"
 
 download() {
-    full_src="${old_data_dir}/$1"
-    full_dst="${data_download_dir}/$2"
-    update_src="${update_src_dir}/$1"
-    update_dst="${update_download_dir}/$2"
-
+    full_hdfs_path="${full_dir}/$1"
+    full_tmp_path="${full_tmp_dir}/$2"
+    update_hdfs_path="${update_src_dir}/$1"
+    update_tmp_path="${update_tmp_dir}/$2"
+    if [[ ! -d "${full_tmp_dir}" ]]; then
+        mkdir -p ${full_tmp_dir}
+    fi
+    if [ ! -d "${update_tmp_dir}" ]; then
+        mkdir -p ${update_tmp_dir}
+    fi
+    if [[ -f "${full_tmp_path}" ]];then
+        echo '' > ${full_tmp_path}
+        mv ${full_tmp_path} /tmp/${2}
+    fi
+    if [[ -f "${update_tmp_path}" ]];then
+        echo '' > ${update_tmp_path}
+        mv ${update_tmp_path} /tmp/${2}
+    fi
+    echo "download file from ${full_hdfs_path} to local path ${full_tmp_path} ..."
+    hdfs dfs -getmerge ${full_hdfs_path}  ${full_tmp_path}
+    echo "download update file from ${update_hdfs_path} to local path ${update_tmp_path} ..."
+    hdfs dfs -cat ${update_hdfs_path} | tr "${old_delimiter}" "${delimiter}" > ${update_tmp_path}
 }
 
 upload() {
 
+}
+
+clean_tmp() {
+    full_tmp_path="${full_dir}/${1}"
+    update_tmp_path="${update_tmp_dir}/${1}"
+    merged_file="${full_tmp_path}_merged"
+    echo "表${1}处理完成，开始清理环境..."
+    if [[ -f "${full_tmp_path}" ]];then
+        echo '' > ${full_tmp_path}
+        mv ${full_tmp_path} /tmp/${1}
+    fi
+    if [[ -f "${update_tmp_path}" ]];then
+        echo '' > ${update_tmp_path}
+        mv ${update_tmp_path} /tmp/${1}
+    fi
+    if [[ -f "$merged_file" ]]; then
+        echo '' > ${merged_file}
+        mv ${merged_file} /tmp/${1}
+    fi
 }
