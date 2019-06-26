9a01e9c4d6823315828817fb8fe14a1cb28b1a69
deadwind4 <rsl4@foxmail.com>
2019-06-26 21:41:39 +0800

    update scala JdbcDemo

--- core/src/main/scala/org/gumiho/demo/JdbcDemo.scala
+++ core/src/main/scala/org/gumiho/demo/JdbcDemo.scala
@@ -1,10 +1,28 @@
 import java.sql.DriverManager
 
-object JdbcDemo {
+import scala.io.Source
 
+object JdbcDemo {
     def main(args: Array[String]): Unit = {
+        val host = ""
+        val db = ""
+        val user = ""
+        val password = ""
         classOf[com.mysql.jdbc.Driver]
-        val url = "jdbc:mysql://localhost:3306/foo_db?user=foo&password=bar"
+        val url = s"jdbc:mysql://${host}:3306/${db}?user=${user}&password=${password}"
         val conn = DriverManager.getConnection(url)
+        val r = Source.fromFile("" )
+        for (l <- r.getLines()) {
+            val tableName = l.trim()
+            try {
+                val statement = conn.createStatement()
+                val sql = s""
+                val resultSet = statement.executeUpdate(sql)
+                println(resultSet)
+            } catch {
+                case e:Throwable => e.printStackTrace()
+            }
+        }
+        conn.close()
     }
 }
