531769e7e459bce48448d7282e0ccef3d73c17c8
deadwind4 <rsl4@foxmail.com>
2019-07-11 00:25:23 +0800

    add loggin demo

--- py/ml/main/main.py
+++ py/ml/main/main.py
@@ -1,8 +1,11 @@
 import torch
 import re
+import logging
+logging.basicConfig(level=logging.DEBUG)
 x = torch.eye(3)
 print(x)
 
-r=re.match('2'
-           '', '123')
+r=re.match('sys_apply', 'sys_apply')
 print(r)
+
+logging.debug("a")
