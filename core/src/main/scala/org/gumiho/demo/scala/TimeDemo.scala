c3d694242b815535363bbd38af150838c0df370b
deadwind4 <rsl4@foxmail.com>
2019-07-24 11:14:05 +0800

    add demo

--- core/src/main/scala/org/gumiho/demo/scala/TimeDemo.scala
+++ core/src/main/scala/org/gumiho/demo/scala/TimeDemo.scala
@@ -6,10 +6,11 @@ import java.time.format.DateTimeFormatter
 object TimeDemo {
     def main(args: Array[String]): Unit = {
         val str = "2019-07-20 19:30:04"
-        val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
-        println(LocalDateTime.now().format(formatter))
         val fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-        val date = LocalDateTime.parse(str, fmt).toLocalDate
+        val date = LocalDateTime.parse(str, fmt)
+            .plusDays(1)
+            .toLocalDate
+            .toString
         println(date)
     }
 }
